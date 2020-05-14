e8f8f2b4a9df3106572236d23b802356b08aa9b9
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-05-14 20:02:54 -0300

    Instalando o PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {TabViewModule} from 'primeng';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        TabViewModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
