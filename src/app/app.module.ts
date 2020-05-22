f4a5ce76f362aae347ac147d2cd494f1776091ce
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-05-22 16:02:27 -0300

    Adicionando o botao de selecao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {ButtonModule, CalendarModule, InputTextareaModule, InputTextModule, TableModule, TooltipModule} from 'primeng';
+import {ButtonModule, CalendarModule, InputTextareaModule, InputTextModule, SelectButtonModule, TableModule, TooltipModule} from 'primeng';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
@@ -17,16 +17,17 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     PessoasPesquisaComponent,
     LancamentoCadastroComponent,
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    InputTextModule,
-    ButtonModule,
-    TableModule,
-    TooltipModule,
-    InputTextareaModule,
-    CalendarModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        InputTextModule,
+        ButtonModule,
+        TableModule,
+        TooltipModule,
+        InputTextareaModule,
+        CalendarModule,
+        SelectButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-lancamento-cadastro',
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentoCadastroComponent implements OnInit {
 
-  constructor() { }
+  tipos = [
+    {label: 'Receita', value: 'RECEITA'},
+    {label: 'Despesa', value: 'DESPESA'}
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
